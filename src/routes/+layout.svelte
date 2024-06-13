dee49c97644951440e7b4757822f24b44057493d
James Ethridge <jamesethridge@Jamess-MacBook-Pro.local>
2024-06-13 15:10:01 -0500

    images added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,31 @@
-<header class="py-4">
-    <Navbar let:NavContainer color="primary">
-        <NavContainer style="align-items: center" class="border px-5 py-2 rounded-lg">
-            <NavBrand href="/">
-                <h1 class="text-2xl font-bold">James Ethridge</h1>
-            </NavBrand>
-            <NavHamburger />
-            <NavUl class="ml-auto">
-                <NavLi href="/">Home</NavLi>
-                <NavLi href="/about">About</NavLi>
-                <NavLi href="/projects">Projects</NavLi>
-                <NavLi href="/contact">Contact</NavLi>
-            </NavUl>
-            <DarkMode />
-        </NavContainer>
-    </Navbar>
-    
-</header>
-
-
-<slot></slot>
+<body class="text-primary-700 dark:text-primary-300 border dark:border-gray-800" >
+    <div class="wrapper">
+        
+        <main>
+            <slot></slot>
+        </main>
+        
+        <!-- Footer -->
+        <footer class="footer py-4 mt-8">
+            <div class="container mx-auto px-4">
+                <p>&copy; 2024 James Ethridge's Portfolio Site. All rights reserved.</p>
+            </div>
+        </footer>
+    </div>
+</body>
 
 <script>
     import "../app.css";
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
-    import { DarkMode } from 'flowbite-svelte';
 </script>
 
 <style>
-
+    footer {
+        height: 85%;
+        margin: -10px;
+    }
+    .wrapper {
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        min-height: 100vh;
+    }
 </style>
